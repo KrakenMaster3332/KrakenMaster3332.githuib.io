165303c38cd52633b48fd56fda57c707d061eef2
KrakenMaster3332 <88001593+KrakenMaster3332@users.noreply.github.com>
2021-07-28 12:33:36 -0700

    Add files via upload

---
+++ SeaSerpent/code.js
@@ -0,0 +1,15 @@
+function GoForward(){
+    console.log ("going forward")
+}
+function TurnRight(){
+    console.log ("turning right")
+}
+function TurnLeft(){
+    console.log ("turning left")
+}
+function GoBackward(){
+    console.log ("going backward")
+}
+function AllStop(){
+    console.log ("stop everything")
+}
